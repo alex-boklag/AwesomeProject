675fc377c118a658f70abc1484f4db910642bcde
Oleksandr Boklah <sanya32102@gmail.com>
2020-04-26 21:45:11 +0300

    Add splash screen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import SplashScreen from 'react-native-splash-screen';
 import Gallery from './src/containers/Gallery';
 import UploadPhoto from './src/containers/UploadPhoto';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
