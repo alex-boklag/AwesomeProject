6d0b7f056fd226559fd2aeb529c1a1d9aa322e66
Oleksandr Boklah <sanya32102@gmail.com>
2020-04-27 20:16:34 +0300

    Change path to configureStore

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
-
-import configureStore from './src/store';
+import {configureStore} from './src/store/configureStore';
 
 const store = configureStore();
 
