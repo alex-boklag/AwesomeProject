d927d5ec767fb9f7ab9eca28fcf07b13657a305f
Oleksandr Boklah <sanya32102@gmail.com>
2020-04-27 20:27:48 +0300

    Update Gallery

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,24 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {View, Text, Image, ScrollView} from 'react-native';
-import {useSelector} from 'react-redux';
+import {Layout} from '../components/Layout';
+import {fetchImages} from '../actions/imagesActions';
+import {axios} from '../services/mockApi';
 
 export default function Gallery() {
+  const dispatch = useDispatch();
   const images = useSelector(state => state.imagesReducer.images);
 
+  useEffect(() => {
+    dispatch(fetchImages({axios, api: '/images'}));
+  }, []);
+
   return (
-    <ScrollView>
-      {images.map(item => {
-        return (
-          <View>
-            <Image
-              source={{uri: item.uri}}
-              style={{width: 400, height: 400}}
-              key={item.date}
-            />
-            <Text>{item.date}</Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+    <Layout>
+      <ScrollView>
+        {images.map(item => {
+          return (
+            <View key={item.id}>
+              <Image
+                source={{uri: item.uri}}
+                style={{width: 400, height: 400}}
+              />
+              <Text>{item.date}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </Layout>
   );
 }
