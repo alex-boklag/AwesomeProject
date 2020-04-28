981694692b446cd9a9e5b8ac658672f6ebfd98b9
Oleksandr Boklah <sanya32102@gmail.com>
2020-04-28 17:01:37 +0300

    Stylize & restructure Gallery

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,7 +1,8 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {View, Text, Image, ScrollView} from 'react-native';
+import {ScrollView} from 'react-native';
 import {Layout} from '../components/Layout';
+import {PhotoCard} from '../components/PhotoCard';
 import {fetchImages} from '../actions/imagesActions';
 import {axios} from '../services/mockApi';
 
@@ -17,15 +18,7 @@ export default function Gallery() {
     <Layout>
       <ScrollView>
         {images.map(item => {
-          return (
-            <View key={item.id}>
-              <Image
-                source={{uri: item.uri}}
-                style={{width: 400, height: 400}}
-              />
-              <Text>{item.date}</Text>
-            </View>
-          );
+          return <PhotoCard key={item.id} item={item} />;
         })}
       </ScrollView>
     </Layout>
